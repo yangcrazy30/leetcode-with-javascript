258b44417213eae4f8915290356746732b877254
Zeo <yangcrazy30@hotmail.com>
2019-05-31 15:34:21 +0800

    update:leetcode 2019-5-31

---
+++ 606.根据二叉树创建字符串.js
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode.cn id=606 lang=javascript
+ *
+ * [606] 根据二叉树创建字符串
+ *
+ * https://leetcode-cn.com/problems/construct-string-from-binary-tree/description/
+ *
+ * algorithms
+ * Easy (49.21%)
+ * Likes:    62
+ * Dislikes: 0
+ * Total Accepted:    3.7K
+ * Total Submissions: 7.4K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * 你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+ * 
+ * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: 二叉树: [1,2,3,4]
+ * ⁠      1
+ * ⁠    /   \
+ * ⁠   2     3
+ * ⁠  /    
+ * ⁠ 4     
+ * 
+ * 输出: "1(2(4))(3)"
+ * 
+ * 解释: 原本将是“1(2(4)())(3())”，
+ * 在你省略所有不必要的空括号对之后，
+ * 它将是“1(2(4))(3)”。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: 二叉树: [1,2,3,null,4]
+ * ⁠      1
+ * ⁠    /   \
+ * ⁠   2     3
+ * ⁠    \  
+ * ⁠     4 
+ * 
+ * 输出: "1(2()(4))(3)"
+ * 
+ * 解释: 和第一个示例相似，
+ * 除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t
+ * @return {string}
+ */
+var tree2str = function(t) {
+    var res = '';
+    function dfs(root){
+        if(!root) return;
+        res += root.val;
+        if(!root.left && root.right){
+            res += '()';
+            res += '(';
+            dfs(root.right);
+            res += ')';
+        }else if(root.left && !root.right){
+            res += '(';
+            dfs(root.left);
+            res += ')';
+        }else if(root.left && root.right){
+            res += '(';
+            dfs(root.left);
+            res += ')';
+            res += '(';
+            dfs(root.right);
+            res += ')';
+        }
+    }
+    dfs(t);
+    return res;
+};
+
