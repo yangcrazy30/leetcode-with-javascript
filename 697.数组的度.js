a68617ea705acb48a020e5dceac61e98860b6930
Zeo <yangcrazy30@hotmail.com>
2019-06-11 14:30:52 +0800

    update:2019-6-11

--- 697.数组的度.js
+++ 697.数组的度.js
@@ -49,6 +49,25 @@
  * @return {number}
  */
 var findShortestSubArray = function(nums) {
-    
+    if(nums.length < 2) return nums.length;
+    let obj = {};
+    let max = { key: null, val: 0, len: 0 };
+    for(let i=0,len=nums.length;i<len;i++) {
+        if(obj[nums[i]]) {
+            obj[nums[i]].push(i);
+            let len = obj[nums[i]].length;
+            if((len > max.val) || ((len == max.val) && (i - obj[nums[i]][0] + 1 < max.len))) {
+                max = {
+                    key: nums[i],
+                    val: len,
+                    len: i - obj[nums[i]][0] + 1
+                };
+            }
+        } else {
+            obj[nums[i]] = [i];
+        }
+    }
+    if(max.key == null)return 1;
+    return max.len;
 };
 
